e8b687cf613806f1c39d659e262226449df55643
FutbolProinfo <futbolproinfo@gmail.com>
2025-07-21 15:56:46 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function toggleAccessibility() {
+    document.body.classList.toggle('high-contrast');
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Aquí pots afegir més funcionalitats si vols
+});
